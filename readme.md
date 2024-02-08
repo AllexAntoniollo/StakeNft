f9f9007d58ebbbc8d8c63ceeb59a845f1f57e6f2
Allex Antoniollo <antoniolloallex@gmail.com>
2024-02-08 17:54:27 -0300

    readme

---
+++ readme.md
@@ -0,0 +1,33 @@
+# ITRUST - Stake NFT Protocol
+
+Welcome to ITRUST, an innovative NFT staking protocol that introduces a novel approach to incentivize and reward stakeholders using the earnings generated from our cutting-edge video platform. This protocol seamlessly integrates non-fungible tokens (NFTs) with staking mechanisms, creating a unique ecosystem that benefits both content creators and token holders.
+
+## Softwares
+
+- [Next.js](https://nextjs.org/)
+- [Solidity](https://soliditylang.org/)
+- [Hardhat.js](https://hardhat.org/)
+- [TypeScript](https://www.typescriptlang.org/)
+
+## Contracts
+
+Deployed in Binance Smart Chain network.
+
+- ITrustStake.sol StakeNFT
+
+  Address: 0xCe285BcDDe89F90C72dE58C5A608e75b59174fD5
+  https://sepolia.etherscan.io/address/0xCe285BcDDe89F90C72dE58C5A608e75b59174fD5#code
+
+- ITrustCollection.sol Collection NFT
+
+  Address: 0x3F39888AaF83A31b8F76a5923ffcA7102b75C690
+  https://sepolia.etherscan.io/address/0x3F39888AaF83A31b8F76a5923ffcA7102b75C690#code
+
+## Author
+
+- Allex Antoniollo
+
+## Screenshots
+
+![Home](./screenshots/home.png)
+![Stake](./screenshots/stake.png)
--- stakenft/src/services/Web3Service.ts
+++ stakenft/src/services/Web3Service.ts
@@ -5,102 +5,147 @@ import CollectionAbi from "./Collection.abi.json";
 const STAKE_ADDRESS = `${process.env.STAKE_ADDRESS}`;
 const COLLECTION_ADDRESS = `${process.env.COLLECTION_ADDRESS}`;
 const CHAIN_ID = `${process.env.CHAIN_ID}`;
+
 export type NFT = {
   owner: string;
   tokenId: BigInt;
 };
+
 function getProvider(): ethers.BrowserProvider {
   if (!window.ethereum) throw new Error("No MetaMask found");
   return new ethers.BrowserProvider(window.ethereum);
 }
 
-export async function doLogin() {
-  const provider = getProvider();
-  const account = await provider.send("eth_requestAccounts", []);
-  if (!account || !account.length) throw new Error("Wallet not found/allowed.");
-  await provider.send("wallet_switchEthereumChain", [{ chainId: CHAIN_ID }]);
-
-  return account[0];
+export async function doLogin(): Promise<string> {
+  try {
+    const provider = getProvider();
+    const account = await provider.send("eth_requestAccounts", []);
+    if (!account || !account.length)
+      throw new Error("Wallet not found/allowed.");
+    await provider.send("wallet_switchEthereumChain", [{ chainId: CHAIN_ID }]);
+    return account[0];
+  } catch (error) {
+    console.error("Error during login:", error);
+    throw error;
+  }
 }
 
 export async function myNFTs(): Promise<NFT[]> {
-  const provider = await getProvider();
-  const signer = await provider.getSigner();
+  try {
+    const provider = await getProvider();
+    const signer = await provider.getSigner();
+    const stakeContract = new ethers.Contract(
+      STAKE_ADDRESS,
+      StakeAbi,
+      provider
+    );
 
-  const stakeContract = new ethers.Contract(STAKE_ADDRESS, StakeAbi, provider);
+    const items: NFT[] = await stakeContract.fetchMyNfts({
+      from: signer.address,
+    });
 
-  const items: NFT[] = await stakeContract.fetchMyNfts({
-    from: signer.address,
-  });
-  const nfts: NFT[] = await Promise.all(
-    items.map(async (item: NFT) => {
-      return {
-        owner: item.owner,
-        tokenId: item.tokenId,
-      };
-    })
-  );
+    const nfts: NFT[] = await Promise.all(
+      items.map(async (item: NFT) => {
+        return {
+          owner: item.owner,
+          tokenId: item.tokenId,
+        };
+      })
+    );
 
-  return nfts;
-}
-
-export async function totalStake(): Promise<BigInt> {
-  const provider = await getProvider();
-  const stakeContract = new ethers.Contract(STAKE_ADDRESS, StakeAbi, provider);
-
-  const total: BigInt = await stakeContract.totalStake();
-  return total;
-}
-
-export async function withdraw(tokenId: number) {
-  const provider = await getProvider();
-  const signer = await provider.getSigner();
-
-  const stakeContract = new ethers.Contract(STAKE_ADDRESS, StakeAbi, signer);
-
-  await stakeContract.withdraw(tokenId);
-
-  return;
-}
-
-export async function earned(address: string): Promise<bigint> {
-  const provider = await getProvider();
-  const signer = await provider.getSigner();
-
-  const stakeContract = new ethers.Contract(STAKE_ADDRESS, StakeAbi, signer);
-  const value: bigint = await stakeContract.earned(address);
-
-  return value;
-}
-
-export async function stake(tokenId: number) {
-  const provider = await getProvider();
-  const signer = await provider.getSigner();
-
-  const nftContract = new ethers.Contract(
-    COLLECTION_ADDRESS,
-    CollectionAbi,
-    signer
-  );
-  const isApproved = await nftContract.getApproved(tokenId);
-  if (isApproved !== true) {
-    const txApprove = await nftContract.approve(STAKE_ADDRESS, tokenId);
-    await txApprove.wait();
+    return nfts;
+  } catch (error) {
+    console.error("Error fetching NFTs:", error);
+    throw error;
   }
-  const stakeContract = new ethers.Contract(STAKE_ADDRESS, StakeAbi, signer);
-
-  const tx = await stakeContract.stake(tokenId);
-  tx.wait();
-  return;
 }
 
-export async function harvest() {
-  const provider = await getProvider();
-  const signer = await provider.getSigner();
+export async function totalStake(): Promise<BigInt | undefined> {
+  try {
+    const provider = await getProvider();
 
-  const stakeContract = new ethers.Contract(STAKE_ADDRESS, StakeAbi, signer);
+    if (!provider) {
+      console.error("No provider available.");
+      return undefined;
+    }
 
-  const tx = await stakeContract.getReward();
-  tx.wait();
-  return;
+    const stakeContract = new ethers.Contract(
+      STAKE_ADDRESS,
+      StakeAbi,
+      provider
+    );
+    const total: BigInt = await stakeContract.totalStake();
+
+    return total;
+  } catch (error) {
+    console.error("Error getting total stake:", error);
+    return undefined;
+  }
+}
+
+export async function withdraw(tokenId: number): Promise<void> {
+  try {
+    const provider = await getProvider();
+    const signer = await provider.getSigner();
+    const stakeContract = new ethers.Contract(STAKE_ADDRESS, StakeAbi, signer);
+
+    await stakeContract.withdraw(tokenId);
+  } catch (error) {
+    console.error("Error during withdrawal:", error);
+    throw error;
+  }
+}
+
+export async function earned(address: string): Promise<bigint | undefined> {
+  try {
+    const provider = await getProvider();
+    const signer = await provider.getSigner();
+    const stakeContract = new ethers.Contract(STAKE_ADDRESS, StakeAbi, signer);
+
+    const value: bigint = await stakeContract.earned(address);
+    return value;
+  } catch (error) {
+    console.error("Error retrieving earnings:", error);
+    return undefined;
+  }
+}
+
+export async function stake(tokenId: number): Promise<void> {
+  try {
+    const provider = await getProvider();
+    const signer = await provider.getSigner();
+    const nftContract = new ethers.Contract(
+      COLLECTION_ADDRESS,
+      CollectionAbi,
+      signer
+    );
+
+    const isApproved = await nftContract.getApproved(tokenId);
+
+    if (isApproved !== true) {
+      const txApprove = await nftContract.approve(STAKE_ADDRESS, tokenId);
+      await txApprove.wait();
+    }
+
+    const stakeContract = new ethers.Contract(STAKE_ADDRESS, StakeAbi, signer);
+    const tx = await stakeContract.stake(tokenId);
+    await tx.wait();
+  } catch (error) {
+    console.error("Error during staking:", error);
+    throw error;
+  }
+}
+
+export async function harvest(): Promise<void> {
+  try {
+    const provider = await getProvider();
+    const signer = await provider.getSigner();
+    const stakeContract = new ethers.Contract(STAKE_ADDRESS, StakeAbi, signer);
+
+    const tx = await stakeContract.getReward();
+    await tx.wait();
+  } catch (error) {
+    console.error("Error during harvesting:", error);
+    throw error;
+  }
 }
